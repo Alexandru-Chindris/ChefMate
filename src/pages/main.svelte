c49fc3706e188c50cc612a5e1a23b073630d06c7
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2025-02-11 19:28:51 +0100

    Main View (Recipe detail (only in fetured scrollable section))

--- src/pages/main.svelte
+++ src/pages/main.svelte
@@ -2,42 +2,80 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
-  <Popup opened={showPopup} onPopupClosed={closeRecipeModal}>
-    <Page>
-      <Navbar title={selectedRecipe ? selectedRecipe.title : 'Ricetta'}>
-        <Link slot="nav-right" popupClose>Chiudi</Link>
-      </Navbar>
-      <Block>
-        {#if selectedRecipe}
-          <img src={selectedRecipe.cover} alt={selectedRecipe.title} style="width: 100%" />
-          <p>Visite: {selectedRecipe.views}</p>
-          <p>Likes: {selectedRecipe.likes}</p>
-        {:else}
-          <p>No details</p>
-        {/if}
-      </Block>
-    </Page>
-  </Popup>
+<Popup opened={showPopup} onPopupClosed={closeRecipeModal}>
+  <Page class="container-detail">
+    <Navbar title={selectedRecipe ? selectedRecipe.title : 'Page not found'} class="detail-navbar">
+      <Link slot="nav-left" popupClose><Icon f7="arrow_uturn_left" /></Link>
+      <Link slot="nav-right"><Icon f7="heart" /></Link>
+    </Navbar>
+    <Block class="detail-main-border">
+      {#if selectedRecipe && selectedRecipe.ingridients}
+        <Block>
+          <div class="detail-img-container" style="--selectedRecipe-cover: url({selectedRecipe.cover});">
+            <img src={selectedRecipe.cover} alt={selectedRecipe.title}/>
+          </div>
+          <div class="recipe-info">
+            <h3>{selectedRecipe.title}</h3>
+            <p>{selectedRecipe.category || 'Default Category'}</p>
+            <div class="detail-post-row">
+              <div class="detail-post-item">
+                <Icon f7="eye" size="17px" /> 
+                <span> {selectedRecipe.views}</span>
+              </div>
+              <div class="detail-post-item">
+                <Icon f7="hand_thumbsup" size="17px" />
+                <span> {selectedRecipe.likes}</span>
+              </div>
+              <div class="detail-post-item">
+                <Icon f7="person" size="17px" />
+                <span> {selectedRecipe.author}</span>
+              </div>
+            </div>
+          </div>
+          <div class="stats-row">
+            <div class="stat-item">
+              <Icon f7="flame" size="18px" color="black" />
+              <p>{selectedRecipe.views} cal</p>
+            </div>
+            <div class="stat-item">
+              <Icon f7="person_2" size="18px" color="black" />
+              <p>Servings {selectedRecipe.serving}</p>
+            </div>
+            <div class="stat-item">
+              <Icon f7="timer" size="18px" color="black" />
+              <p>{selectedRecipe.minutes} min</p>
+            </div>
+            <div class="stat-item">
+              <Icon f7="info_circle" size="18px" color="black" />
+              <p>Nutrition</p>
+            </div>
+          </div>
+          <h3>Ingredients</h3>
+          <div class="detail-recipe-components">
+            {#each selectedRecipe.ingridients as item}
+              <div class="detail-recipe-components-item">
+                <img src={item.cover} alt={item.name} />
+                <div class="detail-recipe-components-item-text">
+                  <span>{item.name}</span>
+                  <span>{item.kg}</span>
+                </div>
+              </div>
+            {/each}
+          </div>
+          <Block>
+
+          </Block>
+        </Block>
+      {:else}
+        <p>No details</p>
+      {/if}
+    </Block>
+  </Page>
+</Popup>
   <Toolbar tabbar icons position={"bottom"}>
-    <Link
-      tabLink="#tab-1"
-      tabLinkActive
-      text="Home"
-      iconIos="f7:house"
-      iconMd="material:home"
-    />
-    <Link
-     tabLink="#tab-2"
-     text="Discover"
-     iconIos="f7:search"
-     iconMd="material:search" 
-     />
-    <Link
-      tabLink="#tab-3"
-      text="Profile"
-      iconIos="f7:person"
-      iconMd="material:group"
-    />
+    <Link tabLink="#tab-1" tabLinkActive text="Home" iconIos="f7:house" iconMd="material:home"/>
+    <Link tabLink="#tab-2" text="Discover" iconIos="f7:search" iconMd="material:search" />
+    <Link tabLink="#tab-3" text="Profile" iconIos="f7:person" iconMd="material:group"/>
   </Toolbar>
     <Tabs>
       <Tab id="tab-1" class="page-content" tabActive>
@@ -146,94 +184,289 @@ import '../css/mainView.css';
 
 let today_selection = "Spaghetti di Ludo";
 
-let items = [ 
+
+// Logical View
+let ingridients = [
+  {
+    name: "salt",
+    kg: "23.9g",
+    cover: "../images/ingridients/salt.jpg"
+  },
+  {
+    name: "butter",
+    kg: "3.9g",
+    cover: "../images/ingridients/butter.jpg"
+  },
+]
+
+let items = [
   {
     title: 'Carbonara',
     cover: '../images/images/IMG_0819-843347201.jpg',
-    views: 6062,
-    likes: 18,
+    views: 5322,
+    likes: 14,
+    serving: 4,
+    minutes: 20,
+    author: 'Mario Rossi',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Spaghetti di Ludo',
     cover: '../images/images/spaghetti_ludo.jpg',
-    views: 4500,
-    likes: 25,
+    views: 3745,
+    likes: 10,
+    serving: 4,
+    minutes: 15,
+    author: 'Luigi Bianchi',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt 2",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter 2",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Billie Jean',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-3.jpg',
-    views: 3200,
-    likes: 12,
+    views: 2987,
+    likes: 13,
+    serving: 2,
+    minutes: 30,
+    author: 'Giulia Verdi',
+    category: 'Dessert',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Elemento 4',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-3.jpg',
-    views: 1500,
-    likes: 8,
+    views: 4120,
+    likes: 7,
+    serving: 5,
+    minutes: 25,
+    author: 'Carla Neri',
+    category: 'Appetizer',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Pesto Genovese',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-4.jpg',
-    views: 2890,
+    views: 3666,
     likes: 11,
+    serving: 3,
+    minutes: 18,
+    author: 'Roberto Di Luca',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Riso alla Cantonese',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-5.jpg',
-    views: 2050,
+    views: 2854,
     likes: 16,
+    serving: 4,
+    minutes: 25,
+    author: 'Mario Rossi',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Pollo al Curry',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-6.jpg',
-    views: 3100,
+    views: 3231,
     likes: 19,
+    serving: 2,
+    minutes: 22,
+    author: 'Carla Neri',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Pizza Montanara',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-2.jpg',
-    views: 4640,
-    likes: 24,
+    views: 4977,
+    likes: 18,
+    serving: 4,
+    minutes: 15,
+    author: 'Giulia Verdi',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Tiramisù Classico',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-7.jpg',
-    views: 5270,
-    likes: 31,
+    views: 5230,
+    likes: 28,
+    serving: 6,
+    minutes: 30,
+    author: 'Luigi Bianchi',
+    category: 'Dessert',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Cous Cous di Verdure',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-8.jpg',
-    views: 1790,
+    views: 1867,
     likes: 10,
+    serving: 3,
+    minutes: 18,
+    author: 'Roberto Di Luca',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Salmone al Limone',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-9.jpg',
-    views: 1990,
-    likes: 14,
+    views: 2543,
+    likes: 12,
+    serving: 4,
+    minutes: 23,
+    author: 'Giulia Verdi',
+    category: 'Main Course',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
   {
     title: 'Focaccia alle Erbe',
     cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-10.jpg',
-    views: 3205,
+    views: 3120,
     likes: 17,
+    serving: 3,
+    minutes: 20,
+    author: 'Carla Neri',
+    category: 'Appetizer',
+    ingridients:[
+      {
+        name: "salt",
+        kg: "23.9g",
+        cover: "../images/ingridients/salt.jpg"
+      },
+      {
+        name: "butter",
+        kg: "3.9g",
+        cover: "../images/ingridients/butter.jpg"
+      },],
   },
 ];
 
 /* Recipe Popup View start */
 let selectedRecipe = null;
-  let showPopup = false;
+let showPopup = false;
 
-  function openRecipeModal(item) {
-    selectedRecipe = item;
-    showPopup = true;
-  }
+function openRecipeModal(item) {
+  selectedRecipe = item;
+  showPopup = true;
+}
 
-  function closeRecipeModal() {
-    showPopup = false;
-    selectedRecipe = null;
-  }
+function closeRecipeModal() {
+  showPopup = false;
+  selectedRecipe = null;
+}
 /* Recipe Popup View end */
 
 function greetUserByTime() {
